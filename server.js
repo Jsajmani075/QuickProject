e8d9f1ae09383e9191bcb6208d2241deecf6c71c
Jsajmani075 <jagjotajmani075@gmail.com>
2024-11-10 21:23:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -99,8 +99,10 @@ dotenv.config();
 //databse config
 connectDB();
 
-const __filename=fileURLToPath{import.meta.url};
-const __dirname=path.dirname{__filename};
+const __filename = fileURLToPath(import.meta.url);
+
+// __dirname equivalent
+const __dirname = path.dirname(__filename);
 //rest object
 const app = express();
 
